0348740569c57aed492f541320c9975960961177
mnaibei <muchajulius@gmail.com>
2023-05-14 22:25:37 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
